843b0345284c7553ab9acc9ffeac73967dd725f5
Jhonatan Alves <jhon_fla@hotmail.com>
2018-03-23 00:42:03 -0300

    Removing unused code

--- main.c
+++ main.c
@@ -22,21 +22,6 @@ int max(int a, int b) {
     return b;
 }
 
-int min(int a, int b) {
-    if (a < b)
-        return a;
-    return b;
-}
-
-int pixel_igual(Pixel p1, Pixel p2) {
-    if (p1.r == p2.r &&
-        p1.g == p2.g &&
-        p1.b == p2.b)
-        return 1;
-    return 0;
-}
-
-
 Image escala_de_cinza(Image img) {
     /*for (unsigned int i = 0; i < img.height; ++i) {
         for (unsigned int j = 0; j < img.width; ++j) {
